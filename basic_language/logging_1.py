67666d20d0e8a42851678e014a794ac3ea79f4e0
Clemens Novak <>
2023-06-22 14:18:50 +0200

    updated / enhanced logging

--- basic_language/logging_1.py
+++ basic_language/logging_1.py
@@ -1,8 +1,16 @@
 import logging
 
+
+def f1(x):
+    logging.info("inside the f1(x) function")
+    logging.info("called with x = {}".format(x))
+    return(x+1)
+                 
+
+
 logging.basicConfig(format='%(asctime)-15s - %(filename)s:%(lineno)d - %(funcName)s - %(levelname)s - %(message)s', level=logging.INFO)
 logging.warning('This will get logged as warning')
 
 logging.info('This will get logged as info')
 
-
+f1(4)
