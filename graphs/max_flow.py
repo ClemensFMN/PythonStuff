2aa305cd6c3f719e91b452c5620252c870e23c22
Clemens Novak <clemens@familie-novak.net>
2024-09-22 20:59:39 +0200

    graph algs

--- graphs/max_flow.py
+++ graphs/max_flow.py
@@ -3,8 +3,14 @@
 """
 Created on Thu Jul  2 20:34:28 2020
 
+Create a directed graph with capacities
+Obtain the maximum flow between two vertices
+
 @author: clnovak
 """
+
+
+
 
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -27,7 +33,15 @@ for e in list(G.edges):
 for e, datadict in list(G.edges.items()):
     print(e, datadict, datadict['capacity'])
 
-
+# f is the max flow between 1 & 6
+# F contains the actual flows between nodes of the max-flow solution
 f, F = nx.maximum_flow(G, 1, 6)
 
-p=nx.draw(G, with_labels=True)
+
+# p=nx.draw(G, with_labels=True)
+pos = nx.spring_layout(G, seed=7)
+nx.draw_networkx_nodes(G, pos)
+nx.draw_networkx_edges(G, pos)
+nx.draw_networkx_labels(G, pos)
+edge_labels = nx.get_edge_attributes(G, "capacity")
+nx.draw_networkx_edge_labels(G, pos, edge_labels)
--- graphs/sssp_bellmanford.py
+++ graphs/sssp_bellmanford.py
@@ -12,9 +12,18 @@ G.add_edges_from([(1,2,{'weight': 1}),
                 (3,6,{'weight': 5}),
                 (5,6,{'weight': 1})])
 
+pos = nx.spring_layout(G, seed=7)
+nx.draw_networkx_nodes(G, pos)
+nx.draw_networkx_edges(G, pos)
+nx.draw_networkx_labels(G, pos)
+edge_labels = nx.get_edge_attributes(G, "weight")
+nx.draw_networkx_edge_labels(G, pos, edge_labels)
+
 length, path = nx.single_source_bellman_ford(G, source=1)
 
+
+
 for i in range(1,7):
-    print("Target = ", i, "Length= ", length[i], "Path= ", path[i])
+    print("Target = ", i, "Length = ", length[i], "Path = ", path[i])
 
 
