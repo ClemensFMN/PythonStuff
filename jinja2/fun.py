b3454025a35cbea3ec909485c0fcbad08b398105
Clemens Novak <clemens@familie-novak.net>
2026-05-28 12:00:15 +0200

    jinja, list template variables

--- jinja2/fun.py
+++ jinja2/fun.py
@@ -5,7 +5,7 @@ Created on Thu Oct 12 08:58:47 2023
 @author: 700001473
 """
 
-from jinja2 import *
+from jinja2 import Environment, FileSystemLoader, StrictUndefined, meta
 
 # inside the environment, we can define what should happen when a template variable is not provided (default action is to replace with an empty string which may not always be the best option...)
 env = Environment(
@@ -20,3 +20,14 @@ s = template.render({'var1': 'Hello',
                      'lst': [1,4,10,2],
                      'flag': True})
 print(s)
+
+
+
+
+# We want to extract all template variables
+# get the source first
+source, filename, uptodate = env.loader.get_source(env, "fun.tmpl")
+ast = env.parse(source)
+variables = meta.find_undeclared_variables(ast)
+print("Variables in the template:", variables)
+
--- jinja2/simple.py
+++ jinja2/simple.py
@@ -1,4 +1,4 @@
-from jinja2 import *
+from jinja2 import Environment, FileSystemLoader, StrictUndefined, meta
 
 # inside the environment, we can define what should happen when a template variable is not provided (default action is to replace with an empty string which may not always be the best option...)
 env = Environment(
@@ -6,9 +6,6 @@ env = Environment(
 
 template = env.get_template("simple.tmpl")
 
+
 s = template.render({'var1': 'Hello', 'var2': 'World'})
 print(s)
-
-s = template.render({'var1': 'Hello'})
-print(s)
-
